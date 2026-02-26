33442618bbd8c9a3970c1775c17d9c9ee5c72ca7
Jesper Kjærgaard <eaa26jekj@students.eaaa.dk>
2026-02-26 15:02:26 +0100

    Colors added

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
-"use strict"
+"use strict";
 
 // Funktion erklæring
 function updateBackground() {
-const width = window.innerWidth;
-    if (width < 640){
-        document.body.style.background = "purple";
-    }
-    else if (width < 760){
-        document.body.style.background = "yellow";
-    }
+  const width = window.innerWidth;
+   
+  if (width < 640) {
+    document.body.style.background = "purple";
+  } 
+  else if (width < 760) {
+    document.body.style.background = "yellow";
+  } 
+  else if (width < 900) {
+    document.body.style.background = "blue";
+  } 
+  else if (width < 1100) {
+    document.body.style.background = "red";
+  } 
+  else if (width < 1300) {
+    document.body.style.background = "green";
+  } 
+  else {
+    document.body.style.background = "lightgray";
+  }
+};
 
-    else {
-        document.body.style.background = "lightgray"; 
-    }
-}
 // Kør ved load
 updateBackground();
 
 // Kør igen når vinduet ændres
 window.addEventListener("resize", updateBackground);
-
